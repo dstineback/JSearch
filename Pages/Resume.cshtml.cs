f1d82d8bcc592d1dfef4e2cdad9d13f2c14627d1
Dan Stineback <dstineback@gmail.com>
2024-07-01 14:13:44 -0700

    Added new page for my Resume.

---
+++ Pages/Resume.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JSearch.Pages
+{
+    public class ResumeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
